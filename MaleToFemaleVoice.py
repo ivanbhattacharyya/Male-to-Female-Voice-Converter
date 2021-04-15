983d0939a541c52b92b7bd176eb331ae121762d6
ivanbhattacharyya <62826974+ivanbhattacharyya@users.noreply.github.com>
2021-04-15 15:08:06 +0530

    Update MaleToFemaleVoice.py

--- MaleToFemaleVoice.py
+++ MaleToFemaleVoice.py
@@ -3,6 +3,8 @@ import speech_recognition as sr
 
 
 r = sr.Recognizer()
+
+# downloaded the audio file in wav format from : https://www.youtube.com/watch?v=WH7hUgdQV3I
 with sr.AudioFile('male voice.wav') as source:
     audio = r.listen(source)
     try:
